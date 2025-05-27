f88ab4f30ebbe547fdd65d81de44dbcaf932ad4e
Mostafa Osman <mostafa233.osman@gmail.com>
2025-05-27 18:47:12 +0300

    Refactor TaskItem widget to support dismissible actions and enhance task display; update pubspec.yaml to include Hive and path_provider dependencies

--- lib/widgets/task_item.dart
+++ lib/widgets/task_item.dart
@@ -15,33 +15,43 @@ class TaskItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
-      elevation: 2,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8),
-        child: ListTile(
-          contentPadding: const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
-          title: Text(
-            task.title,
-            style: TextStyle(
-              fontWeight: FontWeight.w600,
-              decoration:
-                  task.isDone ? TextDecoration.lineThrough : TextDecoration.none,
-              color: task.isDone ? Colors.grey : Colors.black,
+    return Dismissible(
+      key: Key(task.title + task.dueDate.toString()),
+      direction: DismissDirection.endToStart,
+      background: Container(
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        color: Colors.redAccent,
+        child: const Icon(Icons.delete, color: Colors.white),
+      ),
+      onDismissed: (_) => onDelete(),
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+        elevation: 2,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: ListTile(
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
+            title: Text(
+              task.title,
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                decoration: task.isDone
+                    ? TextDecoration.lineThrough
+                    : TextDecoration.none,
+                color: task.isDone ? Colors.grey : Colors.black,
+              ),
             ),
-          ),
-          subtitle: task.dueDate != null
-              ? Text('Due: ${task.dueDate!.toLocal().toString().split(' ')[0]}')
-              : null,
-          leading: Checkbox(
-            value: task.isDone,
-            onChanged: (_) => onToggle(),
-          ),
-          trailing: IconButton(
-            icon: const Icon(Icons.delete_outline, color: Colors.redAccent),
-            onPressed: onDelete,
+            subtitle: task.dueDate != null
+                ? Text('Due: ${task.dueDate!.toLocal().toString().split(' ')[0]}')
+                : null,
+            leading: Checkbox(
+              value: task.isDone,
+              onChanged: (_) => onToggle(),
+            ),
+            trailing: const Icon(Icons.drag_handle),
           ),
         ),
       ),
